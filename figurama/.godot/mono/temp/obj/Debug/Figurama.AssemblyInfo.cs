8a9ed130f9f605ff6c7ccb0e6c859c1fd3a6db52
AleDec5 <alejandrodecur@gmail.com>
2026-08-26 18:55:01 -0300

    Jerarquía de clases

--- figurama/.godot/mono/temp/obj/Debug/Figurama.AssemblyInfo.cs
+++ figurama/.godot/mono/temp/obj/Debug/Figurama.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Figurama")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6f2258a5664aee0974d0377b05f895d04a1b77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e004061c31997f1faf2fcf6f74021200dc2e36")]
 [assembly: System.Reflection.AssemblyProductAttribute("Figurama")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Figurama")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
